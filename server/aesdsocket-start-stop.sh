4729b4ea55de2a72a269a2bc0d41271f82144043
Tal.Halpern <talhlp55@gmail.com>
2023-08-04 23:48:46 +0300

    add server/aesdsocket-start-stop.sh

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+case "$1" in
+	start)
+		echo "starting aesdsocket"
+		start-stop-daemon --start --exec /usr/bin/aesdsocket -- d
+		;;
+	stop)
+		echo "stopping aesdsocket"
+		start-stop-daemon -K -n aesdsocket
+		;;
+	*)
+		echo "usage: $0 {start|stop}"
+	exit 1
+esac
+
+
+exit 0
